0965b5c80af2cef3b83d877bd936415d3fc8a900
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-09-08 16:06:27 +0100

    Small refactor on lightUp function and added mouse event

--- vanilla.js
+++ vanilla.js
@@ -2,18 +2,29 @@
 //Function taking a class, hexcode and timer to light up any class
 function lightUp(className, hex, timer) {
     setInterval(function(){
-        document.querySelector(className).style.color = hex; 
+        className.style.color = hex; 
     }, timer);
     setInterval(function(){
-        document.querySelector(className).style.color = "black"; 
+        className.style.color = "black"; 
     }, timer * 2);
 }
 
-lightUp(".html", "#dd4b25", 1000);
-lightUp(".css", "#0d75bb", 1200);
-lightUp(".bs", "#7610f1", 1400);
-lightUp(".js", "#efd81d", 1600);
-lightUp(".npm", "#cb3837", 1800);
-lightUp(".php", "#777bb3", 2000);
-lightUp(".laravel", "#f72c1f", 2200);
+const html = document.querySelector(".html");
+const css = document.querySelector(".css");
+const bs = document.querySelector(".bs");
+const js = document.querySelector(".js");
+const npm = document.querySelector(".npm");
+const php = document.querySelector(".php");
+const laravel = document.querySelector(".laravel");
 
+lightUp(html, "#dd4b25", 1000);
+lightUp(css, "#0d75bb", 1200);
+lightUp(bs, "#7610f1", 1400);
+lightUp(js, "#efd81d", 1600);
+lightUp(npm, "#cb3837", 1800);
+lightUp(php, "#777bb3", 2000);
+lightUp(laravel, "#f72c1f", 2200);
+
+document.querySelector(".html").addEventListener("mouseenter", function() {
+    console.log("Mouse has entered");
+})
